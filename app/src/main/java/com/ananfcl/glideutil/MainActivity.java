d2abda4692f031dc40fc76b6dc300d9d28b98442
fangchaolai <mifangchaolai@163.com>
2017-03-05 12:07:39 +0800

    初次提交，初始化工程

--- app/src/main/java/com/ananfcl/glideutil/MainActivity.java
+++ app/src/main/java/com/ananfcl/glideutil/MainActivity.java
@@ -3,11 +3,15 @@ package com.ananfcl.glideutil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 测试Glide类
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
